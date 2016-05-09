2dc6cb4ea156a7a0d59a0834dca811eb30a97242
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-05-09 20:59:40 +0200

    Added a watch task

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,27 @@
 // gulp plugins
 var gulp     = require('gulp'),
 sass         = require('gulp-sass'),
+path          = require('path'),
+less         = require('gulp-less'),
 gutil        = require('gulp-util'),
-autoprefixer = require('gulp-autoprefixer');
+autoprefixer = require('gulp-autoprefixer'),
+server = require('gulp-server-livereload');
 
-// sass compiler task
-gulp.task('sass', function () {
-    return gulp.src('./src/natal.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./dist'))
-   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'));
+// less compiler task
+gulp.task('less', function () {
+  return gulp.src('./src/natal.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch([ './src/**/*.scss'], ['sass']);
+gulp.task('watch', function() {
+  gulp.watch('./src/*.less', ['less']);
 });
 
-gulp.task('build',['sass'], function () {
+gulp.task('build',['less'], function () {
   gutil.log('Natal has been builded successfull !');
 });
 
