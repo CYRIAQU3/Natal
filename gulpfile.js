a12bab623e73561422b0065c9755df94de3b08d2
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-05-16 19:38:25 +0200

    Added a browser preview for default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,15 @@ path          = require('path'),
 less         = require('gulp-less'),
 gutil        = require('gulp-util'),
 autoprefixer = require('gulp-autoprefixer'),
-server = require('gulp-server-livereload');
+server       = require('gulp-server-livereload'),
+open         = require('gulp-open'),
+os           = require('os');
+
+
+// browser Settings
+var browser = os.platform() === 'linux' ? 'google-chrome' : (
+  os.platform() === 'darwin' ? 'google chrome' : (
+  os.platform() === 'win32' ? 'chrome' : 'firefox'));
 
 // less compiler task
 gulp.task('less', function () {
@@ -21,6 +29,11 @@ gulp.task('less', function () {
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'));
 });
 
+gulp.task('browser', function(){
+  gulp.src('./example/index.html')
+  .pipe(open({app: browser}));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./src/*.less', ['less']);
 });
@@ -29,6 +42,6 @@ gulp.task('build',['less'], function () {
   gutil.log('Natal has been builded successfull !');
 });
 
-gulp.task('default',['build'], function () {
+gulp.task('default',['build','browser','watch'], function () {
   gutil.log('Done');
 });
